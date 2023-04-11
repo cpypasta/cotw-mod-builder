2cdda3743e147dcb7927a9403fdff7f5b45eeadb
rollerb <app.maestro@gmail.com>
2023-04-11 10:50:04 -0500

    skip wheel

--- scripts/pack.bat
+++ scripts/pack.bat
@@ -1,6 +1,3 @@
 echo off
 del /q "%CD%\dist\modbuilder.zip"
 "C:\Program Files\7-Zip\7z.exe" a -tzip "%CD%\dist\modbuilder.zip" "%CD%\dist\modbuilder"
-
-del /q "%CD%\dist\wheel.zip"
-"C:\Program Files\7-Zip\7z.exe" a -tzip "%CD%\dist\wheel.zip" "%CD%\dist\*.whl"
