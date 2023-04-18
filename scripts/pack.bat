2e846c097e9661d9b5c7b0fa82b7a102378cfcae
rollerb <app.maestro@gmail.com>
2023-04-17 21:45:01 -0500

    use 7zip

--- scripts/pack.bat
+++ scripts/pack.bat
@@ -1,3 +1,4 @@
 echo off
 del /q "%CD%\dist\modbuilder.zip"
-"C:\Program Files\7-Zip\7z.exe" a -tzip "%CD%\dist\modbuilder.zip" "%CD%\dist\modbuilder"
+@REM "C:\Program Files\7-Zip\7z.exe" a -tzip "%CD%\dist\modbuilder.zip" "%CD%\dist\modbuilder"
+"C:\Program Files\7-Zip\7z.exe" a "%CD%\dist\modbuilder.7z" "%CD%\dist\modbuilder"
