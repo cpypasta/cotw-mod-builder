ba07fa3ca9c07546a5ad7030d6adbf8cfa8ede5e
rollerb <app.maestro@gmail.com>
2023-04-24 14:30:41 -0500

    start scopes

--- modbuilder/plugins/modify_scopes.py
+++ modbuilder/plugins/modify_scopes.py
@@ -4,8 +4,8 @@ from pathlib import Path
 import PySimpleGUI as sg
 import re, os
 
-NAME = "Decrease Weapon Recoil"
-DESCRIPTION = "Decrease the amount of recoil for all weapons. The weapon names are what is stored in the configuration files, so sorry if they are a bit confusing."
+NAME = "Modify Scope"
+DESCRIPTION = "Modify the range for scopes."
 
 def format_name(name: str) -> str:
   return " ".join([x.capitalize() for x in name.split("_")])
@@ -64,71 +64,16 @@ def get_option_elements() -> sg.Column:
   return sg.Column(layout)
 
 def add_mod(window: sg.Window, values: dict) -> dict:
-  active_tab = window["weapon_recoil_group"].find_currently_active_tab_key().lower() 
-  active_tab = active_tab.split("_")[0]
-  weapon_name = values[f"{active_tab}_weapon"]
-  if not weapon_name:
-    return {
-      "invalid": "Please select weapon first"
-    }
-  
-  weapons = load_weapons()[active_tab]
-  recoil_percent = values[f"{active_tab}_recoil_percent"]
-  weapon = None
-  for w in weapons:
-    if w["name"] == weapon_name:
-      weapon = w
-      break
-  if not weapon:
-    return {
-      "invalid": "The weapon name could not be found"
-    }    
-  weapon_file = weapon["file"]
-  
-  return {
-    "key": f"weapon_recoil_{weapon_name}",
-    "invalid": None,
-    "options": {
-      "name": weapon_name,
-      "recoil_percent": int(recoil_percent),
-      "file": weapon_file
-    }
-  }
+  None
 
 def format(options: dict) -> str:
-  return f"{options['name']} ({options['recoil_percent']}%)"
+  return f"{options['name']}"
 
 def handle_key(mod_key: str) -> bool:
-  return mod_key.startswith("weapon_recoil")
-
-def get_files(options: dict) -> List[str]:
-  return [options["file"]]
-
-def load_archive_files(base_path: Path):
-  archives = {}
-  for file in base_path.glob("*.ee"):
-    archive_files = list(mods.get_sarc_file_info(file).keys())
-    archives[str(file)] = archive_files
-  return archives
-
-def find_archive_files(archive_files: dict, file: str) -> str:
-  found_archives = []
-  for archive, files in archive_files.items():
-    if file in files:
-      found_archives.append(os.path.relpath(archive, mods.APP_DIR_PATH / "org"))
-  return found_archives
+  return mod_key.startswith("scopes")
 
 def merge_files(files: List[str]) -> None:
-  base_path = mods.APP_DIR_PATH / "org" / Path(files[0]).parent.parent
-  archives = load_archive_files(base_path)
-  for file in files:
-    bundle_files = find_archive_files(archives, file)
-    for bundle_file in bundle_files:
-      bundle_lookup = mods.get_sarc_file_info(mods.APP_DIR_PATH / "org" / bundle_file)
-      mods.merge_into_archive(file, bundle_file, bundle_lookup)
+  None
 
 def process(options: dict) -> None:
-  recoil_multiplier = 1 - options["recoil_percent"] / 100
-  file = options["file"]
-  
-  mods.update_file_at_offsets(Path(file), [264, 268, 272, 276], recoil_multiplier, "multiply")
+  None
